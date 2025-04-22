5fb442592339edc681846e78b0293a47170e1379
John Mascarenhas <jmascare@ca.ibm.com>
2025-04-22 10:03:13 -0400

    Update gradio_chat.py

--- app/gradio_chat.py
+++ app/gradio_chat.py
@@ -83,4 +83,4 @@ with gr.Blocks(title="ε.chat v2.0") as demo:
     save_btn.click(fn=save_chat, inputs=state, outputs=None)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
